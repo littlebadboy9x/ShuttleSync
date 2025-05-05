49316bb0758925728edf72ed89bac2b97a7b4dbc
Tran Viet Vuong <105685671+littlebadboy9x@users.noreply.github.com>
2025-05-05 14:00:59 +0700

    Initial clean commit

---
+++ src/main/java/com/example/shuttlesync/ShuttleSyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.shuttlesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuttleSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShuttleSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shuttlesync/ShuttleSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shuttlesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuttleSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
